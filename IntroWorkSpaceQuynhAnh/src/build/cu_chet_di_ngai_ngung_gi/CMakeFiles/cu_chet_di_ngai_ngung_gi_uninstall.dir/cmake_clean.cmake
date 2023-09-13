d7ad4cb836362751cbae122bd2a7780e41363bc5
Joe Lofrese <77900588+joelofrese@users.noreply.github.com>
2023-09-13 08:42:49 -0700

    Delete IntroWorkSpaceQuynhAnh directory

--- IntroWorkSpaceQuynhAnh/src/build/cu_chet_di_ngai_ngung_gi/CMakeFiles/cu_chet_di_ngai_ngung_gi_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/cu_chet_di_ngai_ngung_gi_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/cu_chet_di_ngai_ngung_gi_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- IntroWorkSpaceQuynhAnh/src/build/py_pubsub/prefix_override/sitecustomize.py
+++
@@ -1,3 +0,0 @@
-import sys
-sys.real_prefix = sys.prefix
-sys.prefix = sys.exec_prefix = '/home/haliedo/IntroWorkSpaceQuynhAnh/src/install/py_pubsub'
